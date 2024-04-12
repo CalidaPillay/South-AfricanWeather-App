eaaab12ee344246f9d6f96adc6b623f6264f862c
CalidaPillay <163557194+CalidaPillay@users.noreply.github.com>
2024-04-12 16:08:53 +0200

    added basic css

--- src/app.js
+++ src/app.js
@@ -1,17 +1,53 @@
-function showWeather(response) {}
-
-let apiKey = bdcb7o102f3t06abc42d1da24fda93b2;
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&unit=metric`;
-axios.get(apiUrl).then(showWeather);
-
 function search(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#city");
-  let searchInputElement = search - input.value;
+  let searchInputElement = document.querySelector("#search-input");
+  searchCity(searchInputElement.value);
+}
+function refreshWeather(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temp");
+  let temperature = Math.round(response.data.temperature.current);
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
 
-  let city = document.querySelector("#search-input");
-  city.innerHTML = searchInputElement.value;
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = `${temperature}°c`;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src = "${response.data.condition.icon_url}" class = "icon"/>`;
+}
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}: ${minutes}`;
 }
 
-let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("submit", search);
+function searchCity(city) {
+  let apiKey = "bdcb7o102f3t06abc42d1da24fda93b2";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+searchCity("durban");
